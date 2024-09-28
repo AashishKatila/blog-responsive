b6a4245a4bb60b2f8e3ce517ece10be0dd993730
Aashish Katila <aashishkatila@gmail.com>
2024-09-28 17:08:13 +0545

    Refactor: Remove consoles and fix type

--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -1,28 +1,32 @@
-import { IUseFetchProps } from "@/utils/types";
+import { BlogPost, IUseFetchProps } from "@/utils/types";
 import { useEffect, useState } from "react";
 
 const PROJECT_TOKEN = process.env.NEXT_PUBLIC_MOCK_API_SECRET_KEY;
 
 const useFetch = ({ url = "" }: IUseFetchProps) => {
-  const [data, setData] = useState<any[]>([]);
+  const [data, setData] = useState<BlogPost[]>();
   const [loading, setLoading] = useState<boolean>(true);
-  const [error, setError] = useState<string | null>(null);
+  const [error, setError] = useState<string | any>();
 
   useEffect(() => {
     const fetchData = async () => {
       setLoading(true);
       try {
         const response = await fetch(
-          `https://${PROJECT_TOKEN}.mockapi.io/api/v1/blogs/${url}`
+          `https://${PROJECT_TOKEN}.mockapi.io/api/v1/blogs${url}`
+        );
+        console.log(
+          "url = ",
+          `https://${PROJECT_TOKEN}.mockapi.io/api/v1/blogs${url}`
         );
         if (!response.ok) {
           throw new Error("Network response was not ok");
         }
         const result = await response.json();
-        console.log(result);
+        console.log("Result = ", result);
         setData(result);
       } catch (error) {
-        setError("Error fetching data");
+        setError(error);
         console.error("Error fetching data:", error);
       } finally {
         setLoading(false);
