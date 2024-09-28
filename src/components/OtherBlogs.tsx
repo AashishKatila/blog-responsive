ff3a38b11d80d3a5bc842989b3549320de40841a
Aashish Katila <aashishkatila@gmail.com>
2024-09-28 17:16:38 +0545

    Feat: Fetch data from API

--- src/components/OtherBlogs.tsx
+++ src/components/OtherBlogs.tsx
@@ -1,66 +1,58 @@
-import React from "react";
+"use client";
+
 import BlogCard from "./BlogCard";
-import Button from "./Button";
-import PageControl from "./PageControl";
+import useFetch from "@/hooks/useFetch";
+import { Loader } from "./Loader";
+import Link from "next/link";
+import ErrorComponent from "./ErrorComponent";
 
 const OtherBlogs = () => {
-  const blogPosts = [
-    {
-      imageSource:
-        "https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      date: "Sunday, Jan 01,2023",
-      title: "Foodie Day ",
-      description:
-        " How do you create compelling presentations that wow your colleagues and impress your managers?",
-      labelText: "Food" as "Food",
-    },
-    {
-      imageSource:
-        "https://images.pexels.com/photos/356040/pexels-photo-356040.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      date: "Sunday, Jan 01,2023",
-      title: "Techie Stuff",
-      description:
-        " How do you create compelling presentations that wow your colleagues and impress your managers?",
-      labelText: "Tech" as "Tech",
-    },
-    {
-      imageSource:
-        "https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      date: "Sunday, Jan 01,2023",
-      title: "Foodie Day ",
-      description:
-        " How do you create compelling presentations that wow your colleagues and impress your managers?",
-      labelText: "Food" as "Food",
-    },
-    {
-      imageSource:
-        "https://images.pexels.com/photos/356040/pexels-photo-356040.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      date: "Sunday, Jan 01,2023",
-      title: "Techie Stuff",
-      description:
-        " How do you create compelling presentations that wow your colleagues and impress your managers?",
-      labelText: "Tech" as "Tech",
-    },
-  ];
+  const { data, loading, error } = useFetch({
+    url: "?page=1&limit=4",
+  });
+
+  if (loading) {
+    return (
+      <div className="flex gap-4">
+        <Loader count={4} />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <ErrorComponent error={error} />;
+  }
   return (
     <div className="md:px-32 px-8 pt-10 ">
       <div className="flex justify-between">
-        <h2 className="md:text-2xl text-xl text-offwhite">All Blogs</h2>
+        <h2 className="md:text-2xl text-xl text-offwhite cursor-pointer hover:text-purple transition-all duration-300">
+          <Link href="/blogs">All Blogs</Link>
+        </h2>
         {/* Filter  */}
       </div>
       <div className="flex flex-wrap gap-6 md:gap-0 pt-6 md:pt-0 ">
-        {blogPosts.map((item) => (
-          <BlogCard
-            date={item.date}
-            description={item.description}
-            imageSource={item.imageSource}
-            labelText={item.labelText}
-            title={item.title}
-            className="w-full md:w-1/2 "
-          />
-        ))}
+        {data?.length ? (
+          <>
+            {data.map((item) => (
+              <BlogCard
+                key={item.id}
+                id={item.id || ""}
+                date={item.createdAt}
+                description={item.blog}
+                imageSource={item.image}
+                labelText={item.label}
+                title={item.title}
+                className="w-full md:w-1/2"
+              />
+            ))}
+            <div className="my-6 text-offwhite md:text-2xl text-xl font-medium w-full text-center cursor-pointer hover:text-purple transition-all duration-300 ">
+              <Link href="/blogs">See All Blogs</Link>
+            </div>
+          </>
+        ) : (
+          <div className="text-offwhite mt-4">No Blog Posts Found</div>
+        )}
       </div>
-      <PageControl />
     </div>
   );
 };
