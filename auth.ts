2542aee7a40777579584760f3299feb3c76d43da
Aashish Katila <aashishkatila@gmail.com>
2024-09-28 17:16:51 +0545

    Feat: Add session and callback

--- auth.ts
+++ auth.ts
@@ -33,4 +33,20 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       },
     }),
   ],
+  session: {
+    strategy: 'jwt',
+    maxAge: 24 * 60 * 60, 
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      session.user.id = token.id as string ;
+      return session;
+    },
+  },
 })
