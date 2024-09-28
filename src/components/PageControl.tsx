d69a9426a92de24ac23e359dc8f55062fbd5cd82
Aashish Katila <aashishkatila@gmail.com>
2024-09-28 17:13:45 +0545

    Feat: Add next and prev controls

--- src/components/PageControl.tsx
+++ src/components/PageControl.tsx
@@ -1,11 +1,29 @@
 import React from "react";
 import Button from "./Button";
+import { IPageControl } from "@/utils/types";
 
-const PageControl = () => {
+const PageControl = ({
+  currentPage,
+  lastPage,
+  handleNextPage,
+  handlePrevPage,
+}: IPageControl) => {
   return (
-    <div className="flex w-full pt-4 pb-6 justify-between ">
-      <Button text="Previous" buttonStyle="text-purple" />
-      <Button text="Next" buttonStyle="text-purple" />
+    <div className="flex w-full pt-4 pb-6 justify-between  ">
+      <Button
+        text="Previous"
+        buttonStyle={`text-purple ${
+          currentPage === 1 && "disabled opacity-60 cursor-not-allowed"
+        } `}
+        onClick={handlePrevPage}
+      />
+      <Button
+        text="Next"
+        buttonStyle={`text-purple  ${
+          currentPage === lastPage && "disabled opacity-60 cursor-not-allowed"
+        }`}
+        onClick={handleNextPage}
+      />
     </div>
   );
 };
